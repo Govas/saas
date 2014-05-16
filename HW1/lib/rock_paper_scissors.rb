4fd204824aef747bcfaf215fef79ee3d4ab0c4f5
Arturo Govea <arturogovea@gmail.com>
2014-05-16 18:58:30 -0500

    Part3 HW1 rock_paper_scissors winner method

--- HW1/lib/rock_paper_scissors.rb
+++ HW1/lib/rock_paper_scissors.rb
@@ -4,7 +4,19 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    strategy=["R","P","S"]
+    raise NoSuchStrategyError,"Strategy must be one of R,P,S" unless strategy.include?(player2.at(1))
+    return player1 if player1.at(1)==player2.at(1)
+    case player1.at(1)
+      when "R"
+        return (player2.at(1)=="S")? player1:player2
+      when "P"
+        return (player2.at(1)=="R")? player1:player2
+      when "S"
+        return (player2.at(1)=="P")? player1:player2
+      else
+      raise NoSuchStrategyError,"Strategy must be one of R,P,S"
+    end
   end
 
   def self.tournament_winner(tournament)
